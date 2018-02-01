a0751ebf95fca98a85497027e5c68f621250bfac
ZapAnton <wc3ft2@gmail.com>
2018-02-01 14:35:28 +0300

    Added TranslateSiteParser class

--- webslate.py
+++ webslate.py
@@ -1,14 +1,41 @@
 import urllib.request
+from html.parser import HTMLParser
 
 url = 'https://pl.bab.la/slownik/polski-rosyjski/'
 
 def fetch_html_page(word: str):
     with urllib.request.urlopen(url) as response:
-        htlm = response.read()
+        html = response.read()
 
-        print(htlm)
+        return html.decode('utf-8')
+
+class TranslateSiteParser(HTMLParser):
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+
+        self.is_div_tag = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'div':
+            self.is_div_tag = True
+
+    def handle_endtag(self, tag):
+        if tag == 'div' and self.is_div_tag:
+            self.is_div_tag = False
+
+    def handle_data(self, data):
+        if self.is_div_tag:
+            print(data)
 
 if __name__ == '__main__':
     word = 'język'
 
-    print(fetch_html_page(word))
+    html = fetch_html_page(word)
+
+    html_parser = TranslateSiteParser()
+
+    html_parser.feed(html)
+
+    html_parser.close()
+
